4ef0898dfc298a3987ebbb5dc4d32704b616b6ec
FelizCiano451 <barryallen4223@gmail.com>
2025-05-13 07:25:31 -0300

    Update NotaDeAula.java

--- NotaDeAula.java
+++ NotaDeAula.java
@@ -1,24 +1,51 @@
 package model;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class NotaDeAula extends EntradaPDF {
     private String subtitulo;
     private String disciplina;
     private String instituicao;
-    private int numeroPaginas;
+    private int numPaginas;
 
-    public NotaDeAula(String titulo, List<String> autores, String pathOriginal, String subtitulo,
-                      String disciplina, String instituicao, int numeroPaginas) {
-        super(titulo, autores, pathOriginal);
+    public NotaDeAula(String titulo, List<String> autores, String pathPDF,
+                      String subtitulo, String disciplina, String instituicao, int numPaginas) {
+        super(titulo, autores, pathPDF);
         this.subtitulo = subtitulo;
         this.disciplina = disciplina;
         this.instituicao = instituicao;
-        this.numeroPaginas = numeroPaginas;
+        this.numPaginas = numPaginas;
+    }
+
+    public static NotaDeAula fromLinha(String linha) {
+        String[] partes = linha.split(";");
+        String titulo = partes[1];
+        List<String> autores = Arrays.asList(partes[2].split(","));
+        String pathPDF = partes[3];
+        String subtitulo = partes[4];
+        String disciplina = partes[5];
+        String instituicao = partes[6];
+        int paginas = Integer.parseInt(partes[7]);
+        return new NotaDeAula(titulo, autores, pathPDF, subtitulo, disciplina, instituicao, paginas);
     }
 
     @Override
-    public void salvarNaBiblioteca(String pathBiblioteca) {
-        System.out.println("Salvando nota de aula em " + pathBiblioteca);
+    public String toLinha() {
+        return String.join(";",
+            "NOTA",
+            titulo,
+            String.join(",", autores),
+            pathPDF,
+            subtitulo,
+            disciplina,
+            instituicao != null ? instituicao : "",
+            String.valueOf(numPaginas)
+        );
+    }
+
+    @Override
+    public String getTipo() {
+        return "Nota de Aula";
     }
 }
